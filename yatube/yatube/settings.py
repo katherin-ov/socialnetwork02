97e7673eca83d57bba5a471fad7fd8d24a70e702
Катерина Новикова <katherinov.o@gmail.com>
2022-12-20 13:38:18 +0300

    2 t

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 LOGIN_URL = 'users:login'
-LOGIN_REDIRECT_URL = 'posts:post_detail'
+LOGIN_REDIRECT_URL = 'posts:index'
 
 #  подключаем движок filebased.EmailBackend
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
